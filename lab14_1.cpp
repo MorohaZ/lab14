df50f4646b6b592e9c3ea06f5e38b379d4d84f04
MorohaZ <141795069+MorohaZ@users.noreply.github.com>
2024-01-05 16:28:01 +0700

    complete

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -3,8 +3,29 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
-
+	int i;
+	for ( i = 1; i < N; i++)
+	{
+		T current = d[i];
+		T j= i-1 ;
+		while (j >= 0 && d[j] < current) {
+            d[j + 1] = d[j];
+            j--;
+        }
+        
+		d[j+1]=current;
+		cout<<"Pass "<<i<<":";
+		for ( j = 0; j < N; j++)
+		{
+			cout << d[j]<<" ";
+		}
+		cout << "\n";
+		
+	}
+	
 }
+
+
 
 int main(){
 	int a[10] = {12,25,30,44,2,0,4,7,55,25};
